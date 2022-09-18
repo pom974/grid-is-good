e241491a092261b8dc15b5723b53f0640c862247
Pierre-Olivier <pierrot.mace@gmail.com>
2022-09-18 14:39:58 -0400

    Test commit from Visual Studio

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -9,6 +9,10 @@ namespace Tetris // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            int gitCommitTest = 0;
+
+            gitCommitTest++;
+
             Game game = new Game();
 
             game.Run();
